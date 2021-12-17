41b8501ece6aa7a0eea0c52e5e27e3bf6ef74e9e
akmalhusain <70431817+akmalhusain@users.noreply.github.com>
2021-12-17 20:07:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+class linkedlist
+{
+    static void Main(string[] args)
+    {
+        LinkedList<string> name = new LinkedList<string>();
+        name.AddFirst("akmalhusain");
+        name.AddFirst("shakib");
+        name.AddFirst("sharukh");
+        name.AddFirst("ajmal");
+        name.AddLast("aayat");
+        Console.WriteLine("before remove the linkedlist\n");
+        foreach (string names in name)
+        {
+            Console.WriteLine(names);
+        }
+        name.RemoveFirst();
+        name.RemoveLast();  
+        Console.WriteLine("\n"+"after remove the linkedlist\n");
+        foreach(string names in name)
+        {
+            Console.WriteLine(names);
+        }
+       Console.WriteLine("\n"+"use after and before node\n ");
+        LinkedListNode<string> Node =  name.Find("akmalhusain");
+        name.AddBefore(Node, "sadiya");
+        name.AddAfter(Node, "foziya");
+        foreach (string i in name)
+        { 
+            Console.WriteLine(i);
+        }
+
+
+
+        
+
+    }
+}
